b2445d68ec028a21868e6ef5867864613301708d
nateachino <natealleyne88@gmail.com>
2022-12-06 17:32:22 -0500

    Added name and title state

--- src/App.js
+++ src/App.js
@@ -11,29 +11,57 @@ export let education = [];
 function App() {
   const [workVisible, setWorkVisible] = useState(false);
   const [schoolVisible, setSchoolVisible] = useState(false);
+  const [firstNameInput, setFirstNameInput] = useState("");
+  const [lastNameInput, setLastNameInput] = useState("");
+  const [jobTitleInput, setJobTitleInput] = useState("");
+
+  let fullName = firstNameInput + " " + lastNameInput;
   const workClick = () => {
     setWorkVisible(true);
   };
   const schoolClick = () => {
     setSchoolVisible(true);
   };
+
   return (
     <div className="App">
-      <GeneralInfo />
-      <div className="work-experience">
-        <button onClick={workClick}>+ Add Experience</button>
-        <div>
-          {workVisible ? (
-            <AddExperience vis={setWorkVisible}></AddExperience>
-          ) : null}
+      <div className="half">
+        <GeneralInfo
+          firstName={setFirstNameInput}
+          lastName={setLastNameInput}
+          title={setJobTitleInput}
+        />
+
+        <div className="work-experience">
+          <button onClick={workClick}>+ Add Experience</button>
+          <div>
+            {workVisible ? (
+              <AddExperience vis={setWorkVisible}></AddExperience>
+            ) : null}
+          </div>
+        </div>
+        <div className="school-info">
+          <button onClick={schoolClick}>+ Add School</button>
+          <div>
+            {schoolVisible ? (
+              <AddEducation vis={setSchoolVisible}></AddEducation>
+            ) : null}
+          </div>
         </div>
       </div>
-      <div className="school-info">
-        <button onClick={schoolClick}>+ Add School</button>
-        <div>
-          {schoolVisible ? (
-            <AddEducation vis={setSchoolVisible}></AddEducation>
-          ) : null}
+      <div className="display-cv">
+        <div className="cv">
+          <div className="displayed-top">
+            <div className="displayed-name">{fullName}</div>
+            <div className="job-title">{jobTitleInput}</div>
+          </div>
+          <div className="experience"></div>
+          <div className="info">
+            <div className="contact">
+              Contact
+              <div className="email">testemail@gmail.com</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/General.js
+++ src/components/General.js
@@ -1,15 +1,28 @@
 import React from "react";
+import { useState } from "react";
 
-export function GeneralInfo() {
+export function GeneralInfo(prop) {
+  let setFirstNameInput = prop.firstName;
+  let setLastNameInput = prop.lastName;
+  let setJobTitleInput = prop.title;
+
+  let firstChange = (x) => setFirstNameInput(x.target.value);
+  let lastChange = (x) => setLastNameInput(x.target.value);
+  let titleChange = (x) => setJobTitleInput(x.target.value);
+
   return (
     <div className="general-info">
       <div className="first-name-wrapper">
         <label htmlFor="first-name">First Name</label>
-        <input id="first-name"></input>
+        <input id="first-name" onChange={firstChange}></input>
       </div>
       <div className="last-name-wrapper">
         <label htmlFor="last-name">Last Name</label>
-        <input id="last-name"></input>
+        <input id="last-name" onChange={lastChange}></input>
+      </div>
+      <div className="job-title-wrapper">
+        <label htmlFor="job-title">Job Title</label>
+        <input id="job-title" onChange={titleChange}></input>
       </div>
       <div className="email-wrapper">
         <label htmlFor="email">Email Address</label>
